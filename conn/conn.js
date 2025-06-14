c76f9c21fbb089b4881898b6c0f20e750c962200
Omm Prakash Sahu <opsommprakash@gmail.com>
2025-06-15 00:09:39 +0530

    Update conn.js

--- conn/conn.js
+++ conn/conn.js
@@ -4,8 +4,6 @@ mongoose.set('strictQuery', false);
 const connectToDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
       serverSelectionTimeoutMS: 15000,
       socketTimeoutMS: 20000
     });
