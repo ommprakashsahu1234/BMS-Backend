593bfb8e5b9f8b3e4bceb0355228713ae86898c9
Omm Prakash Sahu <opsommprakash@gmail.com>
2025-06-16 21:19:16 +0530

    first commit

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const app = express();
 app.use(cors());
 app.use(express.json());
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 const { v4: uuidv4 } = require('uuid');
 
 const AccountHolder = require('./models/AccountHolder')
